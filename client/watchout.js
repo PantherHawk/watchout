41b537d84771b471e9be825d50f9f16b8f3bcb20
Alex Rosenthal <alexrosenthalster@gmail.com>
2017-02-18 20:34:15 -0800

    basic reqs complete

--- client/watchout.js
+++ client/watchout.js
@@ -15,7 +15,7 @@ var svg = d3.select(".container").append("svg:svg")
 
 d3.select('body').style('background-image', 'url(bodyBackground.jpg)');
 
-var data = [10, 2, 3, 4, 5];
+var data = [10, 2, 3, 4, 5, 8];
 
 var enemies = svg.selectAll("image").data(data).enter().append("svg:image")
  .attr('x', function(d, i) {return (i + d) * (Math.random() + 60);})
@@ -23,14 +23,22 @@ var enemies = svg.selectAll("image").data(data).enter().append("svg:image")
  .attr("class", "enemies")
  .attr('width', 50)
  .attr('height', 60)
- .attr("xlink:href", "asteroid.png");
+ .attr("xlink:href", "asteroid.png")
+ // .transform("rotate(45 60 60)").translate("100, 100");
 
-var moveEnemies = function() {
-  d3.select('svg').selectAll(".enemies").transition().duration(1000)
+
+
+var moveEnemies = function(element) {
+  element.transition().duration(800).ease('cubic-in-out')
   .attr('x', function(d, i) {return (i  + d) * Math.random() * 60;})
   .attr('y', function(d, i) {return (i + d) * (Math.random() * 40);})
+  .each('end', function() {
+    moveEnemies( d3.select(this) );
+  });
 };
-setInterval(moveEnemies, 1000);
+moveEnemies(enemies);
+
+
 
 var drag = d3.behavior.drag().on('drag', function() {
   player.attr('x', d3.event.x).attr('y', d3.event.y);
@@ -46,7 +54,7 @@ var player = d3.select('svg')
       .attr("xlink:href", "player.png")
       .call(drag);
 
-
+var prevCollision = false;
 var collisionDetection = function() {
   var collision = false;
   score += 1;
@@ -63,29 +71,26 @@ var collisionDetection = function() {
     var enemyDistance = Math.sqrt((posXDiff * posXDiff) + (posYDiff * posYDiff));
 
     // console.log("enemyDistance: " + enemyDistance);
-
+  }
     if ( enemyDistance <= 50 ) {
 
       console.log('hit');
+      collision = true;
 
       if (score > highScore) {
         highScore = score;
       }
       score = 0;
-      collision = true;
-      collisionCounter++;
+      if(prevCollision !== collision) {
+       collisionCounter++;
+      }
+      svg.select('svg').remove();
+      svg.style('background-color', 'red')
       d3.select('.collisions span').text(collisionCounter);
-      d3.select('.highscore span').text(highScore)
-      // d3.selectAll('.container span')
-      // .data(["red"])
-      // .enter()
-      // .append('.container')
-      // .style("background-image", function(d) {
-      //   return d;
-      // })
-      // .exit()
-      // .remove();
+      d3.select('.highscore span').text(highScore);
     }
-  }
+    prevCollision = collision;
 };
-setInterval(collisionDetection, 500);
+
+d3.timer(collisionDetection);
+// setInterval(collisionDetection, 500);
